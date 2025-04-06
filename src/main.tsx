5653285e2bff8c50e0cedb006e0ad811e8e33e5a
Salim Salimzada <salimsalimzada@Salims-MacBook-Pro.local>
2025-04-06 05:42:25 +0400

    chore: add path aliases and configure tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./app";
 import { BrowserRouter } from "react-router";
-
+import "./main.css";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
+import path from "node:path";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@types": path.resolve(__dirname, "src/types"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+      "@services": path.resolve(__dirname, "src/services"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@ui": path.resolve(__dirname, "src/components/ui"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@store": path.resolve(__dirname, "src/store"),
+      "@constants": path.resolve(__dirname, "src/constants"),
+    },
+  },
 });
