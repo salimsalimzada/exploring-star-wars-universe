f4d230b273134c69f5f2ca7cdf7b3a0f469de0da
Salim Salimzada <salimsalimzada@Salims-MacBook-Pro.local>
2025-04-07 08:01:57 +0400

    chore: implement TanStack Query to reduce service fetch boilerplate

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,24 @@ import { createRoot } from "react-dom/client";
 import App from "./app";
 import { BrowserRouter } from "react-router";
 import "./main.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>,
 );
