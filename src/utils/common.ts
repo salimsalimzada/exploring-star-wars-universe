e5262f350e317822e10c1bf523393856ad3e85d5
Salim Salimzada <salimsalimzada@Salims-MacBook-Pro.local>
2025-04-07 08:00:28 +0400

    feat: implement some util functions

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -9,3 +9,32 @@ export class AppError<T> extends Error {
     this.data = data;
   }
 }
+
+export const getElapsedTime = (time: string) => {
+  if (!time) return "";
+  const pastDate = new Date(time);
+  const currentDate = new Date();
+  return `${currentDate.getFullYear() - pastDate.getFullYear()} years ago`;
+};
+
+export const extractIdFromUrl = (url: string): string | null => {
+  const match = url.match(/\/(\d+)\/?$/);
+  return match ? match[1] : null;
+};
+
+export const debounce = <T extends unknown[], R>(
+  callback: (...args: T) => R,
+  delay: number,
+): ((...args: T) => void) => {
+  let timeoutId: NodeJS.Timeout | undefined;
+
+  return (...args: T): void => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+
+    timeoutId = setTimeout(() => {
+      callback(...args);
+    }, delay);
+  };
+};
