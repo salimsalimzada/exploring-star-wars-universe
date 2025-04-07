9c0f02a9334515e45a33ec6af8cd22ca9f95eee7
Salim Salimzada <salimsalimzada@Salims-MacBook-Pro.local>
2025-04-08 00:06:22 +0400

    chore: remove previously defined tanstack configs

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,7 @@ import { BrowserRouter } from "react-router";
 import "./main.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnMount: false,
-      refetchOnWindowFocus: false,
-      staleTime: 1000 * 60 * 5,
-    },
-  },
-});
+const queryClient = new QueryClient({});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
